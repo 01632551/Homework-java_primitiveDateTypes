f0b107360371984ef5e26f66783fe48aaf2439ef
Egor) <kes2079531@gmail.com>
2022-01-19 00:29:14 +0300

    initial commit for 2.1

---
+++ 2.1/src/main.java
@@ -0,0 +1,8 @@
+public class main {
+    public static void main(String[] args) {
+        int PriceOfTicket = 200;
+        int PriceForBonus = 20;
+        int QuantityOfMiles = PriceOfTicket / PriceForBonus;
+        System.out.println(QuantityOfMiles);
+    }
+}
