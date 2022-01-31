5584fd444b87308f8d16e8f689e323995ebeba6f
Egor) <kes2079531@gmail.com>
2022-01-31 18:09:12 +0300

    update Main

---
+++ 2.1/src/Main.java
@@ -0,0 +1,8 @@
+public class Main {
+    public static void main(String[] args) {
+        int priceOfTicket = 200;
+        int priceForBonus = 20;
+        int quantityOfMiles = priceOfTicket / priceForBonus;
+        System.out.println(quantityOfMiles);
+    }
+}
--- 2.1/src/main.java
+++
@@ -1,8 +0,0 @@
-public class main {
-    public static void main(String[] args) {
-        int PriceOfTicket = 200;
-        int PriceForBonus = 20;
-        int QuantityOfMiles = PriceOfTicket / PriceForBonus;
-        System.out.println(QuantityOfMiles);
-    }
-}
